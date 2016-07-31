f08949a25efae9fe07d60fcf840c33345a9691ea
Joshua Pabilona <joshua.pabilona@orangeandbronze.com>
2016-08-01 00:27:15 +0800

    Made request methods of the controllers that use GET, GET

--- src/main/java/com/pnl/controllers/ViewController.java
+++ src/main/java/com/pnl/controllers/ViewController.java
@@ -2,16 +2,17 @@ package com.pnl.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ViewController {
 
-    @RequestMapping("/about")
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
     public String concept() {
         return "about";
     }
 
-    @RequestMapping("/contact-us")
+    @RequestMapping(value = "/contact-us", method = RequestMethod.POST)
     public String contactUs() {
         return "contact_us";
     }
