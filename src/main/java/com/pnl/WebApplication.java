9dd55d3476c3a68a488d527e84804298a62600bc
Joshua Pabilona <joshua.pabilona@orangeandbronze.com>
2016-08-12 17:30:42 +0800

    Updated css/js due to agila's stricter parsing, also modified class WebApplication

--- src/main/java/com/pnl/WebApplication.java
+++ src/main/java/com/pnl/WebApplication.java
@@ -2,11 +2,18 @@ package com.pnl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
 }
